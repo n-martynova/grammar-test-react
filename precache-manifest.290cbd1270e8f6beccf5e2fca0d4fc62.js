4b919b0afd1da4e9bd28f797a21389bc846dac90
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-14 08:48:41 +0000

    Deploy n-martynova/profile-react to github.com/n-martynova/profile-react.git:gh-pages

---
+++ precache-manifest.290cbd1270e8f6beccf5e2fca0d4fc62.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10251c5410e86b81eb39dc84191e7a62",
+    "url": "/profile-react/index.html"
+  },
+  {
+    "revision": "f8cd03a3c2c392762aef",
+    "url": "/profile-react/static/css/main.864602a2.chunk.css"
+  },
+  {
+    "revision": "712cb908346107c1be5c",
+    "url": "/profile-react/static/js/2.4d4b6b53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile-react/static/js/2.4d4b6b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8cd03a3c2c392762aef",
+    "url": "/profile-react/static/js/main.446c8b15.chunk.js"
+  },
+  {
+    "revision": "7d0d6c42c8cd3dd6b4be",
+    "url": "/profile-react/static/js/runtime-main.9122807d.js"
+  },
+  {
+    "revision": "076d851b602b9915c429f3a2e436c639",
+    "url": "/profile-react/static/media/Proxima_300.076d851b.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/profile-react/static/media/Proxima_500.410504d4.otf"
+  },
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "/profile-react/static/media/Proxima_600.6a386899.otf"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/profile-react/static/media/Proxima_700.62d4d7d3.otf"
+  },
+  {
+    "revision": "b4f9eb8ce027016ab9b9860817451d07",
+    "url": "/profile-react/static/media/Proxima_800.b4f9eb8c.otf"
+  },
+  {
+    "revision": "f401366193520cdf512c7ade98260e27",
+    "url": "/profile-react/static/media/Proxima_900.f4013661.otf"
+  }
+]);
